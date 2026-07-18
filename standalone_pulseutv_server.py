2a0e0c989c4027316b072cf3d698e4cdef42ad78
creator911 <yangcheol222@gmail.com>
2026-07-18 20:16:31 +0900

    Refresh platinum shop label styling

--- standalone_pulseutv_server.py
+++ standalone_pulseutv_server.py
@@ -2012,7 +2012,7 @@ def render_dynamic_page(
             "link",
             id="candycast-mobile-style",
             rel="stylesheet",
-            href="/assets/local/candycast-mobile.css?v=20260718-shopcolor2",
+            href="/assets/local/candycast-mobile.css?v=20260718-shopcolor3",
         )
         soup.head.append(mobile_style)
         changed = True
@@ -5641,7 +5641,7 @@ class StandaloneHandler(BaseHTTPRequestHandler):
             "</head>",
             '<link rel="stylesheet" href="/assets/local/candycast-support.css?v=20260717-chat3">'
             '<link rel="stylesheet" href="/assets/local/candycast-member-chat.css?v=20260718-inline3">'
-            '<link rel="stylesheet" href="/assets/local/candycast-mobile.css?v=20260718-shopcolor2">'
+            '<link rel="stylesheet" href="/assets/local/candycast-mobile.css?v=20260718-shopcolor3">'
             '<link rel="stylesheet" href="/assets/local/candycast-restrictions.css"></head>',
         ).replace(
             "</body>",
